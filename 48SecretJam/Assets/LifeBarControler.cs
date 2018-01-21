953c943dbca6f1891d35b92f9267cf310ee2428d
JordanTuffery <tufferyjordan@gmail.com>
2018-01-21 16:26:28 +0100

    finished life-bar

---
+++ 48SecretJam/Assets/LifeBarControler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LifeBarControler : MonoBehaviour {
+
+    private Image bar;
+    private float health;
+
+	// Use this for initialization
+	void Start () {
+        bar = GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (bar == null)
+        {
+            return;
+        }
+        float lastHealth = health;
+        health = LifeBarManager.instance.remainingLife;
+
+        if(health != lastHealth)
+        {
+            bar.fillAmount = health / LifeBarManager.MAX_LIFE_LEVEL;
+        }
+	}
+}
