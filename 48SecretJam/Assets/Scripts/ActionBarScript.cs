144d2204ded6c9632a26a829c5bfd9eed914c101
Courtois <courtois.k2@gmail.com>
2018-01-21 10:53:00 +0100

    Item+AutoActionBar

---
+++ 48SecretJam/Assets/Scripts/ActionBarScript.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+
+public class ActionBarScript : MonoBehaviour {
+
+    public bool[] buttonFilled = new bool[5];
+}
---
+++ 48SecretJam/Assets/Scripts/ActionButtonScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ActionButtonScript : MonoBehaviour {
+
+    public GameObject backGround;
+    public GameObject objectImage;
+    public Sprite Slot_Empty;
+    public Sprite Slot_Full;
+    public Item item;
+
+    private void Start()
+    {
+        if (item == null)
+            EmptyButton();
+        else
+            FillButton(item);
+    }
+
+    public void FillButton(Item item)
+    {
+        backGround.GetComponent<Image>().sprite = Slot_Full;
+        objectImage.GetComponent<Image>().sprite = item.icon;
+        objectImage.GetComponent<Image>().enabled = true;
+        this.item = item;
+    }
+
+    public void EmptyButton()
+    {
+        backGround.GetComponent<Image>().sprite = Slot_Empty;
+        objectImage.GetComponent<Image>().enabled = false;
+        objectImage.GetComponent<Image>().sprite = null;
+        item = null;
+    }
+}
