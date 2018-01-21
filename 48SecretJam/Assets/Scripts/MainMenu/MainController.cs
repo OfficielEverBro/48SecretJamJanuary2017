3b825708a7b27273642f01c97b4588a8eaf8b696
JordanTuffery <tufferyjordan@gmail.com>
2018-01-21 03:47:57 +0100

    Marche stp fdp

---
+++ 48SecretJam/Assets/Scripts/MainMenu/MainController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainController : MonoBehaviour {
+    
+    public void OnPlayButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void OnQuitButton()
+    {
+        Application.Quit();
+    }
+
+}
